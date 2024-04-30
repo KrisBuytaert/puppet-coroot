e2ccfa747ce9aa9459566342461bb98430980dde
Kris Buytaert <sdog@inuits.eu>
2024-04-30 14:05:34 +0200

    Basic coroot runtime

--- manifests/node_agent.pp
+++ manifests/node_agent.pp
@@ -3,6 +3,37 @@
 # A description of what this class does
 #
 # @example
-#   include coroot::node_agent
-class coroot::node_agent {
+#   include coroot::server
+class coroot::node_agent (
+  String $coroot_address   = 'coroot:8080',
+  Boolean $manage_package              = true,
+  String $package_name                 = 'coroot-node-agent',
+  String $scrape_interval   = '15s',
+
+){
+
+  if $manage_package {
+    package {$package_name:
+      ensure => present,
+    }
+  }
+
+  file { '/etc/sysconfig/coroot-node-agent':
+    group   => 0,
+    mode    => '0644',
+    owner   => 0,
+    content => template('coroot/coroot-node-agentt.erb'),
+  }
+
+  service{'coroot-node-agent':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/etc/sysconfig/coroot-node-agent'],
+  }
+
+  systemd::unit_file {'coroot-node-agent.service':
+    source => "puppet:///modules/${module_name}/coroot-node-agent.service"
+  }
+
+
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -4,5 +4,38 @@
 #
 # @example
 #   include coroot::server
-class coroot::server {
+class coroot::server (
+  String $listen_address   = '0.0.0.0:8080',
+  String $bootstrap_clickhouse_address = 'clickhouse:9000',
+  String $bootstrap_prometheus_url     = 'http://prometheus:9090/',
+  String $bootstrap_refresh_interval   = '15s',
+  Boolean $manage_package              = true,
+  String $package_name                 = 'coroot',
+
+){
+
+  if $manage_package {
+    package {$package_name:
+      ensure => present,
+    }
+  }
+
+  file { '/etc/sysconfig/coroot':
+    group   => 0,
+    mode    => '0644',
+    owner   => 0,
+    content => template('coroot/coroot.erb'),
+  }
+
+  service{'coroot':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/etc/sysconfig/coroot'],
+  }
+
+  systemd::unit_file {'coroot.service':
+    source => "puppet:///modules/${module_name}/coroot.service"
+  }
+
+
 }
